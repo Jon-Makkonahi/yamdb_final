7598394ceefc394978e533f6635efcf3f2c1e325
Jon-Makkonahi <danila-bibik2017@yandex.ru>
2022-03-26 11:19:39 +0700

    maybe final

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf.urls import url
+
+schema_view = get_schema_view(
+    openapi.Info(
+      title="Api_yamdb API",
+      default_version='v1',
+      description="Документация  проекта api_yamdb",
+      contact=openapi.Contact(email="admin@api_yamdb.ru"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path(
-        'redoc/',
-        TemplateView.as_view(template_name='redoc.html'),
-        name='redoc'
-    ),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), 
+        name='schema-redoc'),
 ]
